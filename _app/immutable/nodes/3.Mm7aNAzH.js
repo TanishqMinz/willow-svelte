ee72fd1792e458846224b3c24b7d37f8a28ad6e7
Tanishq <tanishqminz@gmail.com>
2024-08-30 20:17:03 +0530

    Updates

--- _app/immutable/nodes/3.Mm7aNAzH.js
+++
@@ -1,1 +0,0 @@
-import{s as i,t as n,d as r,i as c,n as a,f as m}from"../chunks/scheduler.BAfOzc7k.js";import{S as l,i as p}from"../chunks/index.TMB9Ny7t.js";function u(e){let t;return{c(){t=n("this is about")},l(s){t=r(s,"this is about")},m(s,o){c(s,t,o)},p:a,i:a,o:a,d(s){s&&m(t)}}}class h extends l{constructor(t){super(),p(this,t,null,u,i,{})}}export{h as component};
